ef0939d1064a35f3afdcef7206dee15cd72aeeaa
whitequark <whitequark@whitequark.org>
2024-01-30 17:00:05 +0000

    Deploying to gh-pages from @ whitequark/amaranth@ee58c5afa97b8199b25430f99c07361d69b7f5c0 🚀

--- docs/hide-amaranth.hdl-innards/_static/documentation_options.js
+++ docs/hide-amaranth.hdl-innards/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.1.dev52',
+    VERSION: '0.4.1.dev53',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
